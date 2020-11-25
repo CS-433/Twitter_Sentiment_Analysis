95deab0091b2a07a803538d7f9b8835c4e0ddb95
Seifbemus <43467456+Seifbemus@users.noreply.github.com>
2020-11-25 21:08:50 +0100

    Add files via upload

--- glove_template_seif.py
+++ glove_template_seif.py
@@ -3,36 +3,50 @@ from scipy.sparse import *
 import numpy as np
 import pickle
 import random
+import matplotlib.pyplot as plt
+    
+    
+print("loading cooccurrence matrix")
+with open('cooc.pkl', 'rb') as f:
+    cooc = pickle.load(f)
+print("{} nonzero entries".format(cooc.nnz))
 
+nmax = 100
+print("using nmax =", nmax, ", cooc.max() =", cooc.max())
 
-def main():
-    print("loading cooccurrence matrix")
-    with open('cooc.pkl', 'rb') as f:
-        cooc = pickle.load(f)
-    print("{} nonzero entries".format(cooc.nnz))
+print("initializing embeddings")
+embedding_dim = 20
+np.random.seed(1)
+xs = np.random.normal(size=(cooc.shape[0], embedding_dim))
+ys = np.random.normal(size=(cooc.shape[1], embedding_dim))
 
-    nmax = 100
-    print("using nmax =", nmax, ", cooc.max() =", cooc.max())
+eta = 0.0002
+alpha = 3 / 4
 
-    print("initializing embeddings")
-    embedding_dim = 20
-    xs = np.random.normal(size=(cooc.shape[0], embedding_dim))
-    ys = np.random.normal(size=(cooc.shape[1], embedding_dim))
-
-    eta = 0.001
-    alpha = 3 / 4
-
-    epochs = 10
-
-    for epoch in range(epochs):
-        print("epoch {}".format(epoch))
-        for ix, jy, n in zip(cooc.row, cooc.col, cooc.data):
-            logn = np.log(n)
-            fn = min(1.0, (n / nmax) ** alpha)
-            x, y = xs[ix, :], ys[jy, :]
-            scale = 2 * eta * fn * (logn - np.dot(x, y))
-            xs[ix, :] += scale * y
-            ys[jy, :] += scale * x
+epochs = 10
+losses=[]
+for epoch in range(epochs):
+        
+    loss=0
+        
+    voc=cooc.shape[0]
+    scaleX=np.zeros((voc,embedding_dim))
+    scaleY=np.zeros((voc,embedding_dim))
+    print("epoch {}".format(epoch))
+    for ix, jy, n in zip(cooc.row, cooc.col, cooc.data):
+            
+        logn = np.log(n)
+        fn = min(1.0, (n / nmax) ** alpha)
+        x, y = xs[ix, :], ys[jy, :]
+        dot=np.dot(x, y)
+        loss +=fn*((np.dot(x, y)-logn)**2)
+        scaleX[ix] += 2  * fn * ( dot-logn) *y *eta
+        scaleY[jy] += 2  * fn * ( dot-logn) *x *eta
+    xs -= scaleX 
+    ys -= scaleY 
+        
+    print(loss)
+    losses.append(loss)
 
 			# fill in your SGD code here, 
 			# for the update resulting from co-occurence (i,j)
