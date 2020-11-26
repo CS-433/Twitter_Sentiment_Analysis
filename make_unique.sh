1a390b1ff709953d13a46a5210c4b53ded3e0334
lustea0201 <alexandre.luster@epfl.ch>
2020-11-26 15:58:38 +0100

    Remove duplicates from original files

---
+++ make_unique.sh
@@ -0,0 +1,5 @@
+sort train_neg.txt | uniq > train_neg_u.txt
+sort train_pos.txt | uniq > train_pos_u.txt
+sort train_neg_full.txt | uniq > train_neg_full_u.txt
+sort train_pos_full.txt | uniq > train_pos_full_u.txt
+
