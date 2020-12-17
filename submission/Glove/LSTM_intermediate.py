3ab2ba03550bc06de34f7aef315fe4466e00e2e0
Seifbemus <43467456+Seifbemus@users.noreply.github.com>
2020-12-17 12:31:03 +0100

    Add files via upload

---
+++ submission/Glove/LSTM_intermediate.py
@@ -0,0 +1,11 @@
+import wget
+root = 'data/'
+
+
+#Since we wasn't able to save the LSTM model, submission file is downloaded directly 
+def generate_intermediate(intermediate_filename= "tfidf_intermediate.csv"):
+     
+    
+    submission_url = 'https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBclREZ3U5ejdJT1ZqcU02NlRkVzNJMnVvbkRRUHc_ZT00OUVm/root/content'
+    
+    wget.download(submission_url, intermediate_filename)
