b18b51e1ae126478e4196a9ba8dabf83e3ba9ea6
lustea0201 <alexandre.luster@epfl.ch>
2020-12-17 11:57:39 +0100

    Added glove loading

--- submission/run.py
+++ submission/run.py
@@ -2,10 +2,19 @@ from fast_text.fasttext_intermediate import generate_intermediate as gen_fasttex
 from tfidf.tfidf_intermediate import generate_intermediate as gen_tfidf
 from roberta.roberta_intermediate import generate_intermediate as gen_roberta
 import stage2_model as mod
+import wget
 import os
 
 root = 'data/'
 os.makedirs(root, exist_ok=True)
+
+
+print("Step {:d}.\tLoading GloVe".format(step))
+glove_predictions_url = 'https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBclREZ3U5ejdJT1ZqcU01c2Z3VjkwVEhMaXkxZFE_ZT01QXVr/root/content'
+glove_filename = root + 'glove_intermediate.csv'
+wget.download(glove_predictions_url, glove_filename)
+step += 1
+
 
 step = 1
 print("Step {:d}.\tCalling fastText".format(step))
@@ -24,7 +33,7 @@ gen_roberta(roberta_filename)
 step += 1
 
 print("Step {:d}.\tLoading intermediate results".format(step))
-scores = mod.load_all_intermediate(fasttext_filename, tfidf_filename)
+scores = mod.load_all_intermediate(fasttext_filename, tfidf_filename, glove_filename, roberta_filename)
 step += 1
 
 print("Step {:d}.\tGenerating predictions with pre-trained model".format(step))
